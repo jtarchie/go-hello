6e6bb53f010c99bf4f0bf40de31a009ef25e684e
JT Archie <jtarchie@gmail.com>
2016-01-29 09:21:18 -0500

    use environment variable PORT for server

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
