ed3c6cf2027be300b11d9ec0605a6bb10b937db0
Pawel <pawel2-grape@github.com>
2016-01-29 14:41:37 +0100

    This should work?

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+	http.ListenAndServe(":3000", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
